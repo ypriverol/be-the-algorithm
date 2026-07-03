33e7db494a18807c7b5bbbe62f955621d771aa81
Yasset Perez-Riverol <ypriverol@gmail.com>
2026-07-03 10:48:38 +0100

    chore: enable shared leaderboard (set LEADERBOARD_URL)

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // to turn on the class board. Leave it '' and scores stay local-only (board hidden).
 //
 // Setup steps are in docs/leaderboard.md.
-export const LEADERBOARD_URL = '';
+export const LEADERBOARD_URL = 'https://script.google.com/macros/s/AKfycbyKzW2cBekPshO-4VBbjiBYO8np-u_tXIY-byOZwX1n1DfteqGj1zmA-itlMyoL9-rA/exec';
